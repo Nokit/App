167ab0b9d51f46930fb79afd401358707a49e577
Nokit <stmarc.tn@gmail.com>
2018-12-20 16:55:24 +0900

    trying

--- app.js
+++ app.js
@@ -2,7 +2,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
+fs = require('fs');
 
-app.get('/', (req, res) => res.send(/src/getTdnet/Tdnet-rss/Tdnet-rss-20181220.xml));
+let cache = [];
+cache[0] = fs.readFileSync(/src/getTdnet/Tdnet-rss/Tdnet-rss-20181220.xml);
+
+
+app.get('/', (req, res) => {
+   res.setHeader('Content-Type', 'text/xml');
+   res.send(cache[0]);
+
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
