58ef10e25138a49f777813da1433448bdb5d8e6d
Nokit <stmarc.tn@gmail.com>
2018-12-20 22:04:21 +0900

    aaa

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 'use strict';
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
-const fs = require('fs');
+const port = process.env.PORT || 8000;
+var fs = require('fs');
 
-let cache = [];
-cache[0] = fs.readFileSync("src/getTdnet/Tdnet-rss/Tdnet-rss-20181220.xml");
+var stream = fs.createReadStream("src/getTdnet/Tdnet-rss/Tdnet-rss-20181220.xml");
+stream.setEncoding('utf8');
 
+console.log(stream);
 
 app.get('/', (req, res) => {
-   res.setHeader('Content-Type', 'text/xml');
-   res.send(cache[0]);
-
+   res.setHeader('Content-Type', 'text/plain');
+   res.send(stream);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
